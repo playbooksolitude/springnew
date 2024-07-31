0ba84116955a1735e3dcf1957fac72aec9150adc
playbooksolitude <slatopr@gmail.com>
2024-07-31 20:58:51 +0900

    .

--- season_3/4th/diamonds.R
+++ season_3/4th/diamonds.R
@@ -21,3 +21,16 @@ diamonds |>
   ggplot(aes(x = x, y = y)) +
   geom_point(aes(color = cut)) +
   facet_grid(color~cut) 
+
+# git remote set-url origin https://<playbooksolitude>:<ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINLbJka8My817vLoPGuLHDH0CJsScxsNcCle5+4BZ0MH yohanchoi@YOHANui-MacBookPro.local>@github.com/playbooksolitude/springnew.git
+
+
+# git config --global user.name "playbooksolitude"
+# git config --global user.email "slatopr@gmail.com"
+
+
+
+# playbooksolitude for https://github.com
+# Password for https://github.com/hyun98
+
+
