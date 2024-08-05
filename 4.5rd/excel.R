19c7447971dc4f08a19441013ff1e7449360cdf8
playbooksolitude <slatopr@gmail.com>
2024-08-05 18:10:05 +0900

    .

--- 4.5rd/excel.R
+++ 4.5rd/excel.R
@@ -1,7 +1,6 @@
 #24-0804 sun 10:54
 
 # excel
-
 #install.packages('readxl')
 library(readxl)
 #install.packages('writexl')
@@ -10,3 +9,6 @@ library(readxl)
 smiths |> write_xlsx('./season_3/4.5th/smiths.xlsx')
 band_members
 band_instruments
+
+msleep |> 
+  write_xlsx('./season_3/4.5th/msleep.xlsx')
