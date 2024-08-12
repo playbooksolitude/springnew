445bcd8834580ba359c782f1e75f2ec3737a9021
playbooksolitude <slatopr@gmail.com>
2024-08-12 22:39:07 +0900

    .

--- season_3/4.5th/babyname_groupby.R
+++ season_3/4.5th/babyname_groupby.R
@@ -17,6 +17,20 @@ babynames |>
 
 #1-1 1884에 태어난 아이의 수는 몇명인가? ----
 
+# 최근 20년간 가장 많은 이름은?
+babynames |> 
+  group_by(name, year, sex) |> 
+  reframe(n = n()) -> babynames_1groupname
+
+babynames_1groupname |> 
+  pivot_wider(names_from = sex, values_from = n) |> 
+  rowwise() |> 
+  mutate(n = sum(M,F)) |> 
+  drop_na(n) |> 
+  count(name)
+
+babynames |> 
+  filter(name == 'Aaden')
 
 #2 연도별로 태어난 아이의 수를 
 # 성별을 구분해 'baby_1groupby'라는 이름의 변수에 저장하시오 ----
@@ -25,13 +39,18 @@ babynames |>
   group_by(year, sex) |> 
   reframe(sum = sum(n)) -> baby_1groupby)
 
+(baby_1groupby -> baby_2groupby)
+
 #2-1 1884년에 태어난 남자아이와 여자아이의 수는 몇명인가? ----
 
 
 #3 
-factor(baby_1groupby$sex) |> 
-  fct_relevel(c('M', 'F')) -> baby_1groupby$sex
+(factor(baby_1groupby$sex) |> 
+  fct_relevel(c('F', 'M')) -> baby_1groupby$sex)
+(factor(baby_2groupby$sex) |> 
+    fct_relevel(c('', 'M')) -> baby_2groupby$sex)
 
+# M #red ----
 baby_1groupby |> 
   ggplot(aes(x = year, y = sum)) +
   geom_line(aes(color = sex)) +
@@ -40,35 +59,46 @@ baby_1groupby |>
   ggtitle(label = 'USA Babynames') +
   scale_y_continuous(labels = scales::comma) 
 
-# ----
+# levels
+baby_1groupby |> 
+  ggplot(aes(x = year, y = sum)) +
+  geom_line(aes(color = sex), size = .5) +
+  geom_area(alpha = .5, position = 'identity', aes(fill = sex)) +
+  bbc_style() +
+  ggtitle(label = 'USA Babynames') +
+  scale_y_continuous(labels = scales::comma) #+
+  scale_fill_manual(values = c('M' = 'lightblue', 'F' = 'pink')) +
+  scale_color_manual(values = c('M' = 'lightblue', 'F' = 'pink'))
+
+# lightblue ----
 baby_1groupby |> 
   ggplot(aes(x = year, y = sum)) +
   #geom_bar(stat = 'identity')
   geom_line(aes(color = factor(sex,levels = c('F', 'M')), 
                 fill = factor(sex,levels = c('F', 'M')))) +
-  geom_area(alpha = 1, position = 'identity', 
+  geom_area(alpha = .5, position = 'identity', 
             aes(fill = factor(sex,levels = c('F', 'M')))) +
   bbc_style() +
   ggtitle(label = 'USA Babynames') +
-  scale_y_continuous(labels = scales::comma) +
+  scale_y_continuous(labels = scales::comma) #+
   scale_fill_manual(values = c('M' = 'lightblue',
                                'F' = 'lightpink')) +
   scale_color_manual(values = c('M' = 'lightblue',
                                 'F' = 'lightpink'))
 
-?relevel()
+baby_2groupby |> 
+  filter(year == '2000')
 
 #female 순서 변경----
-baby_1groupby |> 
+baby_2groupby |> 
   ggplot(aes(x = year, y = sum)) +
-  #geom_bar(stat = 'identity')
-  geom_line(aes(color = factor(sex,levels = c('M', 'F')), 
-                fill = factor(sex,levels = c('M', 'F')))) +
-  geom_area(alpha = .5, position = 'identity',
-            aes(fill = factor(sex,levels = c('M', 'F')))) +
+  geom_line(aes(color = factor(sex, levels = c('F', 'M')), 
+                fill = factor(sex, levels = c('F', 'M')))) +
+  geom_area(alpha = .5, position = 'identity', 
+            aes(fill = factor(sex, levels = c('F', 'M'))))
   bbc_style() +
   ggtitle(label = 'USA Babynames') +
-  scale_y_continuous(labels = scales::comma) +
+  scale_y_continuous(labels = scales::comma) #+
   scale_fill_manual(values = c('M' = 'lightblue',
                                'F' = 'lightpink')) +
   scale_color_manual(values = c('M' = 'lightblue',
