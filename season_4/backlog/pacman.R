c09aaa36c9f2ef070b14ccb69d1091c68771d54d
playbooksolitude <slatopr@gmail.com>
2024-10-03 11:00:51 +0900

    .

---
+++ season_4/backlog/pacman.R
@@ -0,0 +1,23 @@
+#24-1003 
+
+#
+if(!require(pacman))install.packages("pacman")
+pacman::p_load(
+  "tidyverse", 
+  "googlesheets4", 
+  "scales",
+  "gt", 
+  "showtext", 
+  "nord", 
+  "wesanderson",
+  "ggrepel", 
+  "googlesheets4", 
+  "plotly"
+)
+showtext_auto()
+
+#
+if(!require('devtools'))install.packages("devtools")
+devtools::install_github('bbc/bbplot')
+
+#
