c38c14e6994973ea4454e9bc1ba4b1d7eeae1e0a
playbooksolitude <slatopr@gmail.com>
2025-03-02 09:59:59 +0900

    .

--- 시즌5마지막.R
+++ 시즌5마지막.R
@@ -35,20 +35,21 @@ ap_2dep_rename |> colnames() |> as_tibble() |> as_vector()
 (ap_2arr_rename |> colnames() |> as_tibble() -> ap_2arr_1vector)
 (ap_2dep_rename |> colnames() |> as_tibble() -> ap_2dep_1vector)
 
-#
+# 교집합, 차집합
 intersect(ap_2arr_1vector$value, ap_2dep_1vector$value)
 setdiff(ap_2arr_1vector$value, ap_2dep_1vector$value)
 
 #
-ap_2arr_rename |> 
-  rename(실제시간 = 10) -> ap_3arr_rename
+ap_2arr_rename |> glimpse()
+(ap_2arr_rename |> 
+  rename(실제시간 = 10) -> ap_3arr_rename)
 
-ap_2dep_rename |> 
-  rename(실제시간 = 10) -> ap_3dep_rename
+(ap_2dep_rename |> 
+  rename(실제시간 = 10) -> ap_3dep_rename)
 
 #
-ap_3arr_rename |> colnames() |> as_tibble() -> ap_3arr_rename_q
-ap_3dep_rename |> colnames() |> as_tibble() -> ap_3dep_rename_q
+(ap_3arr_rename |> colnames() |> as_tibble() -> ap_3arr_rename_q)
+(ap_3dep_rename |> colnames() |> as_tibble() -> ap_3dep_rename_q)
 
 #
 setdiff(ap_3arr_rename_q, ap_3dep_rename_q)
@@ -65,27 +66,36 @@ ap_4dep |> slice(1,n())
   mutate(날짜 = ymd(날짜)) -> ap_4arr)
 
 ap_4arr |> slice(1,n())
+ap_4arr |> glimpse()
 
-# ap_4dep |> 
-#   full_join(ap_4arr) -> ap_tidy1
+# 계획시간 변형 ----
+ap_4arr |> 
+  select(날짜, 항공사, 편명, 계획시간, 실제시간) |> 
+  mutate(실제시간edit = paste0(실제시간, ":00")) |> 
+  separate(col = 실제시간edit, into = c('시', '분', '초'), sep = ":") |> 
+  mutate(시 = as.integer(시), 
+         #분2 = as.integer(분), .keep = 'used') |> #sample_n(50) |> print(n =50)
+          분 = as.integer(분), .keep = 'all', 
+         시간 = 시* 60 + 분, 
+         as_hms(시간)) 
+
+
 
 #
 ap_tidy1 |> slice(1,n())
 ap_tidy1 |> glimpse()
 ap_tidy1 |> str()
 
+
+
 #
 ap_3arr_rename |> 
   separate(col = '계획시간', into = c('계획', '시간'), sep = ":") -> temp1
 
+
+
 temp1 |> 
   mutate(시간분 = as.numeric(계획) * 60 + as.numeric(시간), .keep = 'used') |> 
   print(n = 200)
-
-
-
-
-
-
 
 
